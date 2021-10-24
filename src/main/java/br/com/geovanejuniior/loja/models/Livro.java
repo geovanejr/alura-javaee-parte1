1f1b92bf87a436809ad60a82b73c6da57d90ee65
Geovane Junior <geovane.gjunior@gmail.com>
2021-10-24 15:05:28 -0300

    Aula 04 - Adicionando a Data de Publicação

--- src/main/java/br/com/geovanejuniior/loja/models/Livro.java
+++ src/main/java/br/com/geovanejuniior/loja/models/Livro.java
@@ -1,17 +1,22 @@
 package br.com.geovanejuniior.loja.models;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -43,8 +48,10 @@ public class Livro {
 	@Min(20)
 	private Integer numPagina;
 	
+	@Temporal(TemporalType.DATE)
+	private Calendar dataPublicacao =Calendar.getInstance();
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@Size(min=1)
 	@NotNull
 	private List<Autor> autores = new ArrayList<>();
@@ -80,13 +87,15 @@ public class Livro {
 	public void setAutores(List<Autor> autores) {
 		this.autores = autores;
 	}
+	public Calendar getDataPublicacao() {
+		return dataPublicacao;
+	}
+	public void setDataPublicacao(Calendar dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
 	@Override
 	public String toString() {
 		return "Livro [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco
 				+ ", numPagina=" + numPagina + ", autores=" + autores + "]";
 	}
-	
-	
-	
-
 }
